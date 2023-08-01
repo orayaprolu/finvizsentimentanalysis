8d127331e95187abf94dd8895653138363e8491b
orayaprolu <orayaprolu@gmail.com>
2023-08-01 16:36:52 -0700

    Added comments

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# Request module
+# Request module (fetches html content of webpage)
 from urllib.request import urlopen, Request
 
-# Scrapping module
+# Parsing module (converts html file into Pyton object to interact with html elements)
 from bs4 import BeautifulSoup
 
-finvizUrl = "https://finviz.com/quote.ashx?t="
+# Add more websites and tickers later
+finviz_base_url = "https://finviz.com/quote.ashx?t="
 tickers = ["AMD", "AAPL", "GOOGL", "META"]
 
-newsTables = {}
+# Map of ticker and news-table pair
+news_tables = {}
 
+# Goes through each ticker
 for t in tickers:
-    url = finvizUrl + t
+    url = finviz_base_url + t
     
+    # Fetches request and recieves webpage
     req = Request(url= url, headers= {"user-agent": "'my-app"})
     response = urlopen(req)
 
+    # Specifies webpage is html and only returns 'news-table' blocks
     html = BeautifulSoup(response, "html")
     newsTab = html.find(id='news-table')
-    newsTables[t] = newsTab
-
-    break
-
-print(newsTables)
+    news_tables[t] = newsTab
